8fed29b9b7144de7c57148bf23244ecd07e9a0fe
Anuarbek Tuleushov <103310923+Abi1337@users.noreply.github.com>
2023-06-12 05:18:41 +0800

    Add files via upload

---
+++ add-self-assessment.php
@@ -0,0 +1,46 @@
+<?php
+
+    $con = new mysqli('localhost', 'root', '', 'fwdd');
+
+    if($con->connect_error)
+    {
+        echo "Connection to the database has failed !!";
+    }
+
+    if(isset($_POST['addAssessment'])){
+        $moduleID = $_POST['module_id'];
+        $questions = json_decode($_POST['assessment'], true);
+
+        $query = "INSERT INTO assessments (module_id) VALUES ('$moduleID')";
+        $result = mysqli_query($con, $query);
+        
+        $assessmentAdded = false;
+
+        foreach ($questions as $question) {
+            $questionText = $question['questionText'];
+            $choices = $question['choices'];
+            $correctAnswer = $question['correctAnswer'];
+        
+            $query_2 = "INSERT INTO questions (assessment_id, question_title, choices, answer) VALUES ((SELECT assessment_id FROM assessments ORDER BY assessment_id DESC LIMIT 1), ?, ?, ?)";
+            $stmt = $con->prepare($query_2);
+
+            if($stmt) {
+                $stmt->bind_param("sss", $questionText, $choices, $correctAnswer);
+    
+                if($stmt->execute()){
+                    $assessmentAdded = true;
+                } else {
+                    echo "Error Adding Assessment";
+                }
+        
+                $stmt->close();
+            } else {
+                echo "SQL Query Error";
+            }
+        }
+        
+        if($assessmentAdded) {
+            echo "Assessment Has Been Added Successfully";
+        }
+    }
+?>
